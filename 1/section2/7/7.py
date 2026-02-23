475cc10774994c040fecde72bb68d43fe0b1d20b
JisuEun <zkxktmxmfhvl@gmail.com>
2026-02-23 10:04:06 +0900

    .

--- 1/section2/7/7.py
+++ 1/section2/7/7.py
@@ -3,9 +3,4 @@ input = sys.stdin.readline
 
 N = int(input())
 
-cnt = 0
-for i in range(2, N+1):
-    isPrime = False
-    for j in range(2, i):
-        if i%j == 0:
-            isPrime = False
+# Todo:
